cb127e67c140b220ff0497db6e2cafa6b58b23f7
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2019-01-02 21:51:20 +0700

    3

--- Buildx86/core.js
+++ Buildx86/core.js
@@ -1,15 +1,44 @@
+//https://able.bio/drenther/track-page-visibility-in-react-using-render-props--78o9yw5
+
+var _IS_KEY_BACKSPACE_OR_DELETE = false;
+var _IS_KEY_ENTER = false;
+//=====================================================================
 if (document && document.body) document.body.style.display = 'none';
 
 console.log('core.js .....');
 
 
-function __getMeanText() {
+function ___getMeanText() {
     var el = document.querySelector('.text-wrap.tlid-copy-target');
     if (el) return el.textContent;
     return '';
 }
 
-function onDomReady() {
+function ___inputKeydown(e) {
+    var key = e.keyCode || e.charCode;
+    if (key == 8 || key == 46) {
+        ___inputProcess('DELETE_CHAR')
+        return;
+    }
+}
+
+function ___inputKeypress(e) {
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if (code == 13) {
+        ___inputProcess('ENTER')
+        return;
+    }
+    ___inputProcess('DATA')
+}
+
+function ___inputProcess(key) {
+    setTimeout(function () {
+        var _input = document.getElementById('source').value;
+        console.log(key, _input);
+    }, 100);
+}
+
+function ___onDomReady() {
     console.info('DOM loaded');
     //-----------------------------------------------------------------
     var uri_core_css = '[URL_CORE_CSS]';
@@ -25,13 +54,29 @@ function onDomReady() {
     var header = document.getElementById('gb');
     if (header) header.style.display = 'none';
     //-----------------------------------------------------------------
+    //document.body.addEventListener('DOMSubtreeModified', function () {
+    //    var time = 'DOM Changed at ' + new Date();
+    //    console.log(time);
+    //}, false);
+
+    //var elSuggest = document.getElementById('gt-src-is')
+    //if (elSuggest) elSuggest.addEventListener('DOMSubtreeModified', function (event) {
+    //    var source = document.getElementById('source');
+    //    if (source) {
+    //        var _input = source.value;
+    //        console.log(_input);
+    //    }
+    //}, false);
+    //-----------------------------------------------------------------
+    var elSource = document.getElementById('source')
+    if (elSource) {
+        elSource.setAttribute('onkeypress', '___inputKeypress(event, this.value.trim())');
+        elSource.setAttribute('onkeydown', '___inputKeydown(event)');
+    }
+    //-----------------------------------------------------------------
     setTimeout(function () {
         if (document && document.body) document.body.style.display = 'block';
     }, 100);
 }
 
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", onDomReady);
-} else {
-    onDomReady();
-}
+if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", ___onDomReady); else ___onDomReady();
